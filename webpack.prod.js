19b79e7908ee4ff7754742d1eb5d992a2363b312
yogi-25 <yogitamisal12@gmail.com>
2024-06-01 17:57:55 +0530

    Updated package.json

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,23 +1,35 @@
-const path = require('path')
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
 module.exports = {
-entry:{
+  entry: {
     main: './app.js'
-},
-output: {
-path: path.join(__dirname,'prod-build'),
-publicPath: '/',
-filename: '[name].js',
-clean:true
-
-},
-mode: 'production',
-target: 'node',
-module: {
-    rules:[ {
-    test: /\.js$/,
-    exclude: /node_modules/,
-    loader: "babel-loader",
-    }]
-}
-
-}
+  },
+  output: {
+    path: path.join(__dirname, 'prod-build'),
+    publicPath: '/',
+    filename: '[name].js',
+    clean: true
+  },
+  mode: 'production',
+  target: 'node', // Ensures webpack handles node built-in modules correctly
+  externals: [nodeExternals()], // Exclude node_modules from the bundle
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+  stats: {
+    errorDetails: true, // Show error details
+  },
+  ignoreWarnings: [
+    {
+      module: /node_modules\/express\/lib\/view\.js/,
+      message: /the request of a dependency is an expression/,
+    },
+  ],
+};
